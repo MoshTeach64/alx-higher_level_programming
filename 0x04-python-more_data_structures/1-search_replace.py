cfe27774fe64f3debe6f207701f5e4d789379021
Moshood Odutola Ezekiel <odutolamoshood1985@gmail.com>
2022-12-08 06:38:39 -0800

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    len_ = len(my_list)
+    new_list = my_list.copy()
+
+    for i in range(len_):
+        if new_list[i] == search:
+            new_list[i] = replace
+
+        return new_list

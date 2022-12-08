87f3538e02a3b7a535e9a46631dc52705dcf7b2d
Moshood Odutola Ezekiel <odutolamoshood1985@gmail.com>
2022-12-08 10:00:12 -0800

    102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,6 +3,6 @@
 def complex_delete(a_dictionary, value):
     # return {key:val for key, val in a_dictionary.items() if val != value}
     for key, val in dict(a_dictionary).items():
-         if val == value:
+        if val == value:
             del a_dictionary[key]
     return a_dictionary
